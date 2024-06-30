d548a141981919f6135b299787240d407c0f8365
Shane Darilek <swdarilek@gmail.com>
2024-06-29 20:08:23 -0500

    Adding an interactive application

---
+++ src/InteractiveApplication.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class InteractiveApplication {
+    Scanner input = new Scanner(System.in);
+
+
+    static void welcomeUser() {
+        System.out.println("Hello! First things first, who ARE you??");
+
+    }
+
+}
