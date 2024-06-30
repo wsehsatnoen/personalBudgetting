9be40022c7ed12702522dae119a489caf65c6fd1
wsehsatnoen <113001014+wsehsatnoen@users.noreply.github.com>
2024-06-29 20:38:54 -0500

    Update Profile.java
    Added the Instance fields and the constructor.

--- src/Profile.java
+++ src/Profile.java
@@ -1,3 +1,14 @@
 public class Profile {
     
+    private String name;
+    private String email;
+    private String password;
+    private List<Accounts> accountsOwned;
+
+    public Profile(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.accountsOwned = new ArrayList<>();
+    }
 }
