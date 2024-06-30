00b5aa22347a8b9a27361f19f728d040a07aa469
Shane Darilek <shanedarilek@Shanes-MBP.lan>
2024-06-29 23:01:01 -0500

    Added profile functionallity

--- src/InteractiveApplication.java
+++ src/InteractiveApplication.java
@@ -4,9 +4,15 @@ public class InteractiveApplication {
     Scanner input = new Scanner(System.in);
 
 
-    static void welcomeUser() {
-        System.out.println("Hello! First things first, who ARE you??");
-
+     public void welcomeUser() {
+        String email;
+        String password;
+        System.out.print("Hello! First things first, who ARE you??\n email: ");
+        email = input.next();
+        System.out.print("\nPassword: ");
+        password = input.next();
+        System.out.println("\nAttempting log in...");
+        Profile.login(email, password);
     }
 
 }
--- src/Profile.java
+++ src/Profile.java
@@ -1,14 +1,47 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Profile {
-    
-    private String name;
-    private String email;
+
+    private final String name;
+    private final String email;
     private String password;
-    private List<Accounts> accountsOwned;
+    public int passwordAttempts;
+    public boolean isLocked;
+    private final List<Accounts> accountsOwned;
+    private static final Map<String, Profile> profiles = new HashMap<>();
 
     public Profile(String name, String email, String password) {
         this.name = name;
         this.email = email;
         this.password = password;
         this.accountsOwned = new ArrayList<>();
+        this.passwordAttempts = 0;
+        this.isLocked = false;
+        profiles.put(email, this);
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public List<Accounts> getAccountsOwned() {
+        return this.accountsOwned;
+    }
+
+    public static Profile login(String email, String password) {
+        if (profiles.containsKey(email)) {
+            if (profiles.get(email).password.equals(password)) {
+                return profiles.get(email);
+            }
+            return null;
+        }
+        return null;
     }
 }
